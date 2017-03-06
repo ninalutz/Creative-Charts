e8c1fcece11c351ee5a3cddbbb8880add5755d9b
Nina Lutz <nlutz@mit.edu>
2017-03-06 16:20:02 -0500

    dragging circles

--- CreativeCharts/sketch.js
+++ CreativeCharts/sketch.js
@@ -1,9 +1,50 @@
+var cx = 100; 
+var cy = 200;
+var csize = 50;
+ var overC, locked;
+
 function setup() {
-  createCanvas(500, 500);
+  createCanvas(1000, 500);
+  ellipseMode(RADIUS);
+   overC = false;
+ locked = false;
 }
 
 function draw() {
   background(200);
-  fill(255, 0, 0);
-  ellipse(20, 20, 20, 20);
+
+  
+   if (mouseX > cx-csize && mouseX < cx+csize && 
+      mouseY > cy-csize && mouseY < cy+csize) {
+    overC = true;  
+    if(!locked) { 
+      stroke(255); 
+      fill(153);
+    } 
+  } else {
+    stroke(153);
+    fill(255);
+    overC = false;
+  }
+  
+    ellipse(cx, cy, 50, 50);
+}
+
+
+function mousePressed(){
+    if(overC){
+      locked = !locked;
+    }
+  
+}
+function mouseDragged(){
+  if(locked){
+    cx = mouseX;
+    cy = mouseY;
+  }
+  
+}
+
+function mouseReleased(){
+  locked = false;
 }
