43802eab7e7bb79458aff7a3721ba4e9b4cade90
Nina Lutz <nlutz@mit.edu>
2017-03-06 16:34:10 -0500

    not working class

--- CreativeCharts/sketch.js
+++ CreativeCharts/sketch.js
@@ -3,48 +3,70 @@ var cy = 200;
 var csize = 50;
  var overC, locked;
 
+var testC;
+
 function setup() {
   createCanvas(1000, 500);
   ellipseMode(RADIUS);
    overC = false;
  locked = false;
+ testC = new country("USA", 80, createVector(300, 200));
 }
 
 function draw() {
   background(200);
 
-  
-   if (mouseX > cx-csize && mouseX < cx+csize && 
-      mouseY > cy-csize && mouseY < cy+csize) {
-    overC = true;  
-    if(!locked) { 
+    testC.mouseupdate();
+    testC.display();
+}
+
+
+
+
+var country = function(name, size, location){
+  this.name = name;
+  this.x = location.x;
+  this.y = location.y;
+  this.size = size;
+  this.locked = false;
+  this.overC = false;
+}
+
+country.prototype.mouseupdate = function(){
+  if (mouseX > this.x-this.size && mouseX < this.x+this.size && 
+      mouseY > this.y-this.size && mouseY < this.y+this.size) {
+    this.overC = true;  
+    if(!this.locked) { 
       stroke(255); 
       fill(153);
     } 
   } else {
     stroke(153);
     fill(255);
-    overC = false;
+    this.overC = false;
   }
   
-    ellipse(cx, cy, 50, 50);
-}
-
-
-function mousePressed(){
-    if(overC){
-      locked = !locked;
+  function mousePressed(){
+    if(this.overC){
+      this.locked = !this.locked;
     }
-  
 }
+
 function mouseDragged(){
   if(locked){
-    cx = mouseX;
-    cy = mouseY;
+    this.x = mouseX;
+    this.y = mouseY;
   }
-  
 }
 
 function mouseReleased(){
-  locked = false;
+  this.locked = false;
+}
+  
+}
+
+country.prototype.display = function(){
+  
+  ellipse(this.x, this.y, this.size, this.size);
+  
 }
